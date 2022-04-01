57daea6ac763ced3bafe1576234e34e43cf46190
realPep <salikhov@sfedu.ru>
2022-04-01 22:34:27 +0300

    переход на новый api и доработка кэширования

---
+++ FinClever/Models/invest/FinnhubQuote.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace FinClever.Models
+{
+    public class FinnhubQuote
+    {
+        [JsonProperty(PropertyName = "c")]
+        public double CurrentPrice { get; set; }
+    }
+}
---
+++ FinClever/Models/invest/StockHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace FinClever.Models.invest
+{
+    public class StockHistory
+    {
+        [JsonProperty(PropertyName = "Time Series (Daily)")]
+        public Dictionary<string, StockHistoryItem>? Prices { get; set; }
+    }
+
+    public class StockHistoryItem
+    {
+        [JsonProperty(PropertyName = "1. open")]
+        public string? Open { get; set; }
+        [JsonProperty(PropertyName = "2. high")]
+        public string? High { get; set; }
+        [JsonProperty(PropertyName = "3. low")]
+        public string? Low { get; set; }
+        [JsonProperty(PropertyName = "4. close")]
+        public string? Close { get; set; }
+        [JsonProperty(PropertyName = "5. volume")]
+        public string? Volume { get; set; }
+    }
+}
